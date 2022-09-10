d333546cf21f03bf677f3dbe1017a04d0bf5a2b9
BestUazDriver <Sabitov.Daniyar18@mail.ru>
2022-09-10 11:22:24 +0300

    lol

--- src/com/sabitov/Main.java
+++ src/com/sabitov/Main.java
@@ -31,6 +31,12 @@ public class Main {
         long endTime = System.nanoTime();
         long duration = (endTime - startTime);
         System.out.println("Time: " + duration + " milliseconds");
+
+        long startTime1 = System.nanoTime();
+        System.out.println(Arrays.deepToString(squareMatrixMultiply2(matrix1, matrix2)));
+        long endTime1 = System.nanoTime();
+        long duration1 = (endTime1 - startTime1);
+        System.out.println("Time: " + duration1 + " milliseconds");
     }
 
     private static int[][] squareMatrixMultiply(int[][] matrix1, int[][] matrix2) {
@@ -44,4 +50,18 @@ public class Main {
         }
         return matrix;
     }
+
+    private static int[][] squareMatrixMultiply2(int[][] matrix1, int[][] matrix2) {
+        int[][] matrix = new int[matrix1.length][matrix2.length];
+
+        for (int j = 0; j < matrix.length; j++) {
+            for (int i = 0; i < matrix.length; i++) {
+                for (int n = 0; n < matrix.length; n++) {
+                    matrix[i][j] += matrix1[i][n] * matrix2[n][j];
+                }
+            }
+        }
+        return matrix;
+    }
+
 }
